e9ca9602ee61cbc31b21c667da43ef96b7425697
Raaya Pathare <raayapathare7@gmail.com>
2019-09-07 17:35:07 +0530

    updated about section

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Container, Typography, Box } from '@material-ui/core'
 import { AboutStyles } from '../style/muiStyles'
 
@@ -8,12 +9,15 @@ const About = (props) => {
     return (
         <Box className={classes.root}>
             <Box className={classes.background}>
-                <Container>
+                <Container className={classes.content}>
                     <Typography className={classes.aboutText}>
-                        about.
+                        I'm passionate about well designed technology - if you are too, we'll get along. 
+                        Web development allows me to be my most creative while also satisfying my desire to learn. 
+                        On the weekend you'll find me at an easel, with my dog at a far away beach, or scoping out the best dumpling joint in Auckland. 
+                        If you need a little more detail, check out my CV, or <Link to="/contact">get in touch.</Link>
                     </Typography>
                     <Typography className={classes.CVicon}>
-                        Download my CV!
+                        <a href="../files/cv.pdf" target="_blank">Download</a>
                     </Typography>
                     <Typography className={classes.about}>
                         about.
