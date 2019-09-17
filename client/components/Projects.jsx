ac0cbbd00fc98444408597fb4bf71296ad6dce56
Raaya Pathare <raayapathare7@gmail.com>
2019-09-17 16:14:22 +1200

    initial commit

---
+++ client/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+
+const Projects = () => {
+    return (
+        <Grid container>
+            <Typography>
+                working?
+            </Typography>
+        </Grid>
+    )
+}
+
+export default Projects
