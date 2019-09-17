e32c26234aa4511a6dd5f2fbfc3af43acfccde9a
Raaya Pathare <raayapathare7@gmail.com>
2019-09-18 11:13:10 +1200

    moved watch better to its own component for readability

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Typography, Slide } from '@material-ui/core'
 import Typing from 'react-typing-animation'
+import WB from './WB'
 
 
 import { ProjectsStyles } from '../style/muiStyles'
@@ -18,30 +19,7 @@ const Projects = (props) => {
                     </Typing>
                 </Typography>
             </Grid>
-            <Slide direction="up" in={true} timeout={1300} mountOnEnter unmountOnExit>
-            <Grid container className={classes.WBContainer}>
-                <Grid item className={classes.WBphoto}>
-                    
-                </Grid >
-                <Grid item className={classes.WBinfo}>
-                    <Typography className={classes.title}>
-                        watch better. 
-                    </Typography>
-                    <br />
-                    <Typography className={classes.subheading}>
-                        A diverse database for the conscious movie consumer.
-                    </Typography>
-                    <br />
-                    <Typography className={classes.bodytext}>
-                        With watch better, users can browse a vast list of films, filtering through five different diversity tests. Users can also search for and receive information on films, test their favourites to see how they fare against our tests and create an account, allowing them to save their favourites on a watchlist for the next login. 
-                    </Typography>
-                    <br/>
-                    <Typography className={classes.bodytext2}>
-                        ui design / ux design / front end development
-                    </Typography>
-                </Grid >
-            </Grid>
-            </Slide>
+                <WB />
         </Grid>
     )
 }
---
+++ client/components/WB.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Grid, Typography, Slide } from '@material-ui/core'
+import { ProjectsStyles } from '../style/muiStyles';
+
+const WB = (props) => {
+    const classes = ProjectsStyles(props)
+    return (
+        <>
+            <Slide direction="up" in={true} timeout={1300} mountOnEnter unmountOnExit>
+                <Grid container className={classes.WBContainer}>
+                    <Grid item className={classes.WBphoto}>
+
+                    </Grid >
+                    <Grid item className={classes.WBinfo}>
+                        <Typography className={classes.title}>
+                            watch better.
+                    </Typography>
+                        <br />
+                        <Typography className={classes.subheading}>
+                            A diverse database for the conscious movie consumer.
+                    </Typography>
+                        <br />
+                        <Typography className={classes.bodytext}>
+                            With watch better, users can browse a vast list of films, filtering through five different diversity tests. Users can also search for and receive information on films, test their favourites to see how they fare against our tests and create an account, allowing them to save their favourites on a watchlist for the next login.
+                    </Typography>
+                        <br />
+                        <Typography className={classes.bodytext2}>
+                            ui design / ux design / front end development
+                    </Typography>
+                    </Grid >
+                </Grid>
+            </Slide>
+        </>
+    )
+}
+
+export default WB
