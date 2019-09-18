44132123da21ce6e85f65e24ca01846982ffd489
Raaya Pathare <raayapathare7@gmail.com>
2019-09-19 10:45:33 +1200

    watch better link now provided

--- client/components/WB.jsx
+++ client/components/WB.jsx
@@ -22,6 +22,12 @@ const WB = (props) => {
                         <br />
                         <Typography className={classes.bodytext}>
                             With watch better, users can browse a vast list of films, filtering through five different diversity tests. Users can also search for and receive information on films, test their favourites to see how they fare against our tests and create an account, allowing them to save their favourites on a watchlist for the next login.
+                            <br />
+                            <a 
+                            href="http://watchbetter.herokuapp.com/" target="_blank"
+                            style={{ textDecoration: 'none', color: 'white'}}>
+                            Click me.
+                            </a>
                     </Typography>
                         <br />
                     <Typography className={classes.bodytext2}>
