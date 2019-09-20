347416d887ef51eb3cc63520dfae20676ecaa083
Raaya Pathare <raayapathare7@gmail.com>
2019-09-20 16:07:46 +1200

    changed to higher order component and implemented withStyles

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
-import { Grid, TextField, Button, Typography } from '@material-ui/core'
+import { Grid, TextField, Button, Typography, withStyles } from '@material-ui/core'
 import * as emailjs from 'emailjs-com'
 import Typing from 'react-typing-animation'
 
-class Contact extends React.Component {
+const styles = theme => ({
+    root: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        width: '35vw',
+    },
+    headingContainer: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    heading: {
+        fontFamily: 'Hammersmith One, sans-serif',
+        fontSize: '10em',
+        letterSpacing: '-0.1em'
+    },
+    buttonContainer: {
+        display: 'flex', 
+        justifyContent: 'center'
+    },
+    button: {
+        color: '#A100FF', 
+        fontFamily: 'Hammersmith One, sans-serif'
+    },
+    userMessage: {
+        fontFamily: 'Hammersmith One, sans-serif', 
+        color: 'red'
+    }
+})
+const inputProps = {
+    style: { 
+        fontSize: '1.2em', 
+        fontFamily: 'Sorts Mill Goudy, serif', 
+        width: '35vw', 
+    }
+}
+const inputlabelprops = {
+    style: { 
+        fontFamily: 'Sorts Mill Goudy, serif', 
+        color: '#b07009' 
+    }
+}
 
+
+class HigherOrderComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -33,12 +82,12 @@ class Contact extends React.Component {
             to_name: 'raaya.pathare7@gmail.com',
             message_html: this.state.message
         }
-            if (this.state.email !== this.state.confEmail) {
-                this.setState({
-                    emailError: true
-                })
-            } else {
-                emailjs.send('gmail', 'portfolio_contact_form', templateParams, 'user_BakXnlA942EEymoG7yUrf')
+        if (this.state.email !== this.state.confEmail) {
+            this.setState({
+                emailError: true
+            })
+        } else {
+            emailjs.send('gmail', 'portfolio_contact_form', templateParams, 'user_BakXnlA942EEymoG7yUrf')
                 .then(res => {
                     console.log('SUCCESS!', res)
                     this.setState({
@@ -60,99 +109,73 @@ class Contact extends React.Component {
                 confEmail: '',
                 message: '',
             })
-            }        
-    }
-
-    styles = {
-        textField: {
-            width: '35vw',
-            marginBottom: '1.5em'
         }
     }
 
     render() {
+        const { classes } = this.props
         return (
             <form>
-            <Grid container
-                style={{
-                    height: '100vh',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-                <Grid container
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        width: '35vw'
-                    }}>
-                    <Grid item style={{ display: 'flex', justifyContent: 'center'}}>
-                        <Typography style={{
-                            fontFamily: 'Hammersmith One, sans-serif',
-                            fontSize: '10em',
-                            letterSpacing: '-0.1em'
-                        }}>
-                            <Typing hideCursor={true} speed={120}>
-                            Contact.
-                            </Typing>
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <form autocomplete="off">
-                        <TextField 
-                        InputProps={{
-                            style: { fontSize: '1.2em', fontFamily: 'Sorts Mill Goudy, serif', width: '35vw', marginBottom: '2em' }
-                          }}
-                        required name="name" value={this.state.name} onChange={this.handleChange}
-                        label="Name" />
-
-                        <TextField
-                        InputProps={{
-                            style: { fontSize: '1.2em', fontFamily: 'Sorts Mill Goudy, serif', width: '35vw', marginBottom: '2em' }
-                          }}
-                        required name="email" value={this.state.email} onChange={this.handleChange} label="Email" autocomplete="none"/>
-
-                        <TextField
-                        InputProps={{
-                            style: { fontSize: '1.2em', fontFamily: 'Sorts Mill Goudy, serif', width: '35vw', marginBottom: '2em' }
-                          }}
-                        required name="confEmail" value={this.state.confEmail} onChange={this.handleChange} label="Confirm email" />
-
-                        {this.state.emailError &&
-                        <Typography style={{ fontFamily: 'Hammersmith One, sans-serif', color: 'red' }}>
-                            Email addresses do not match. Please check and re-enter.
-                        </Typography>}
-
-                        <TextField
-                        InputProps={{
-                            style: { fontSize: '1.2em', fontFamily: 'Sorts Mill Goudy, serif', width: '35vw', marginBottom: '2em' }
-                          }}
-                        required name="message" multiline={true} rows={7} value={this.state.message} onChange={this.handleChange} label="Message" />
-                        <br />
-                        <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
-                        <Button style={{ color: '#A100FF', fontFamily: 'Hammersmith One, sans-serif' }} type="submit" value="submit" onClick={this.handleSubmit} name="submit">
-                        Submit Message
-                        </Button>
+                <Grid container className={classes.root}>
+                    <Grid container className={classes.content}>
+                        <Grid item className={classes.headingContainer}>
+                            <Typography className={classes.heading}>
+                                <Typing hideCursor={true} speed={120}>
+                                    Contact.
+                                </Typing>
+                            </Typography>
                         </Grid>
-                        <br />
+                        <Grid item>
+                            <form autoComplete="off">
+                                <TextField
+                                    InputProps={{inputProps}}
+                                    InputLabelProps={{inputlabelprops}}
+                                    required name="name" value={this.state.name} onChange={this.handleChange}
+                                    label="Name" autoComplete="none"/>
 
-                        {this.state.confirmation && 
-                        <Typography style={{ fontFamily: 'Hammersmith One, sans-serif', color: 'red' }}>
-                            Message sent successfully.
-                        </Typography>}
+                                <TextField
+                                    InputProps={{inputProps}}
+                                    InputLabelProps={{inputlabelprops}}
+                                    required name="email" value={this.state.email} onChange={this.handleChange} label="Email" autoComplete="none" />
 
-                        {this.state.sendingError &&
-                        <Typography style={{ fontFamily: 'Hammersmith One, sans-serif', color: 'red' }}>
-                            Unfortunately an error occurred. Please try again.
-                        </Typography>}
-                        </form>
+                                <TextField
+                                    InputProps={{inputProps}}
+                                    InputLabelProps={{inputlabelprops}}
+                                    required name="confEmail" value={this.state.confEmail} onChange={this.handleChange} label="Confirm email" autoComplete="none" />
+
+                                {this.state.emailError &&
+                                    <Typography className={classes.userMessage}>
+                                        Email addresses do not match. Please check and re-enter.
+                                    </Typography>}
+
+                                <TextField
+                                    InputProps={{inputProps}}
+                                    InputLabelProps={{inputlabelprops}}
+                                    required name="message" multiline={true} rows={7} value={this.state.message} onChange={this.handleChange} label="Message" autoComplete="none"/>
+                                <br />
+                                <Grid className={classes.buttonContainer}>
+                                    <Button className={classes.button} type="submit" value="submit" onClick={this.handleSubmit} name="submit">
+                                        Submit Message
+                                    </Button>
+                                </Grid>
+                                <br />
+
+                                {this.state.confirmation &&
+                                    <Typography className={classes.userMessage}>
+                                        Message sent successfully.
+                                    </Typography>}
+
+                                {this.state.sendingError &&
+                                    <Typography className={classes.userMessage}>
+                                        Unfortunately an error occurred. Please try again.
+                                    </Typography>}
+                            </form>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
             </form>
         )
     }
 }
 
-export default Contact
+export default withStyles(styles)(HigherOrderComponent)
