6175ff440b841f52dcbf240263264edaa0df6eca
Raaya Pathare <raayapathare7@gmail.com>
2019-09-24 17:57:53 +1200

    added proc file and optimized server port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const server = require('./server')
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
+// server.listen(port, function () {
+//   // eslint-disable-next-line no-console
+//   console.log('Listening on port', port)
+// })
+
+let port = process.env.PORT || 3000
+if (port == null || port == "") {
+  port = 8000;
+}
 server.listen(port, function () {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', port)
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port)
 })
